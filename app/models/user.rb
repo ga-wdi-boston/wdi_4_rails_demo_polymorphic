9aa0de0a5ea6a22060859bd5fc45a36d03771827
Alex Grant <grantovich@users.noreply.github.com>
2014-07-01 12:00:43 -0400

    Add like/unlike links to likeable items

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_many :links, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def like_for(likeable)
+    likeable.likes.find_by(user_id: id)
+  end
 end
